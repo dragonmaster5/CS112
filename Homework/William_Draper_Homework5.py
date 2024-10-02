e16f232f34f225c5ee1942b7060de35ca77ed98a
Billy <Griffindorv@gmail.com>
2024-10-01 20:00:31 -0500

    Finished Homework 5

--- Homework/William_Draper_Homework5.py
+++ Homework/William_Draper_Homework5.py
@@ -3,8 +3,17 @@
  Write a function filter positive even(numbers) that takes a list of integers
 and returns a new list containing only the positive even numbers.
 """
+import math
+
+from In_Class.InClassWeek5 import alist
+
+
 def filterEven(alist):
-    return[i for i in alist if i>0 and i%2==0]
+    blist=[]
+    for i in alist:
+        if i%2==0 and i>0:
+            blist.append(i)
+    return blist
 
 print(filterEven([1 , -2 , -3 , 4 , 5 , 6]))
 #p2
@@ -23,8 +32,10 @@ Write a function named multiply factor(a list, factor) that returns a list where
 each element in the original list is multiplied by the given factor. For example
 multiply_factor ([1 , 2 , -3] , 2)'''
 def multiplyFactor(alist,factor):
-
-    return [i*factor for i in alist]
+    blist=[]
+    for i in alist:
+        blist.append(i*factor)
+    return blist
 
 print(multiplyFactor([1 , 2 , -3] , 2))
 #p4
@@ -34,7 +45,11 @@ and floating-point numbers. For example:numeric_values (["1", " apple ", 1 , 1.2
 should return [1, 1.2, −4].
 '''
 def numericValues(alist):
-    return [i for i in alist if type(i) is type(0) or type(i) is type(0.0)]
+    blist=[]
+    for i in alist:
+        if type(i) is type(0) or type(i) is type(0.0):
+            blist.append(i)
+    return blist
 print(numericValues(["1", " apple ", 1 , 1.2 , -4]))
 
 #p5
@@ -43,13 +58,21 @@ an element as input and returns a new list with all occurrences of that element 
 For example
 removed_element ([0 , " test ", 1 , " apple ", 0 , 1.1] , 0)'''
 def removedElement(alist,ele):
-    return[i for i in alist if i!=ele]
+    blist=[]
+    for i in alist:
+        if i != ele:
+            blist.append(i)
+    return blist
+
+
 print(removedElement([0 , " test ", 1 , " apple ", 0 , 1.1],0))
 #p6
 '''. Write a function uppercase strings(a list) that takes a list of strings and
 returns a new list with all the strings in uppercase. For example'''
 def uppercaseStrings(alist):
-    return[i.upper() for i in alist]
+    blist=[]
+    for i in alist:
+        blist.append(i.upper())
 #P7
 '''
 Write a function last names(full names) that takes a list of full names and
@@ -61,7 +84,10 @@ Alan Turing "])
 should return [“Nguyen”, “Hilbert”, “Jordan”, ”Turing”]. All of these individuals are
 famous, except for the first one (at least for now!).'''
 def lastName(fullNames):
-    return [i.split()[-1] for i in fullNames]
+    blist=[]
+    for i in fullNames:
+        blist.append(i.split()[-1])
+    return blist
 print(lastName(["Anne Nguyen", "David Hilbert", "Michael Jordan", "Alan Turing"]))
 #p8
 '''Write a function join strings(a list) that takes a list of strings and joins them
@@ -73,3 +99,46 @@ def joinStrings(alist):
     for i in alist:
         concatString+=i
     return concatString
+#p9
+'''Write a function longest string(a list) that takes a list of strings and returns
+the longest string. For example
+longest_string ([" apple ", " banana ", " mango "])
+should return “banana”.'''
+def longestString(alist):
+     maxa=0
+     for i in range(0,len(alist)):
+         if (len(alist[i]))>len(alist[maxa]):
+             maxa=i
+     return alist[maxa]
+print(longestString([" apple ", " banana ", " mango "]))
+#p10
+'''
+Given a list of circle radii, create a new list containing the corresponding
+areas of the circles, rounded to one decimal place. For example, if the list is
+radii = [1 ,2 ,3]
+''' import math
+list1=[1,2,3]
+list2=[round(math.pi*(i**2),1) for i in list1]
+print(list2)
+#p11
+'''From a list of integers, create a new list that includes only the odd numbers
+using list comprehension. For example, if the given list is [1, 2, 3, 4, 5, 6], then the new
+list should be [1, 3, 5]'''
+intlist=[1,2,3,4,5,6]
+intlist2=[i for i in intlist if i%2!=0]
+print(intlist2)
+#p12
+'''2. From a list of strings, create a new list that contains the first letter of each
+string. For example, if the given list is [“Apple”, “Banana”, “Mango”], the new list should
+be [“A”, “B”, “M”].
+'''
+stringList=["Apple", "Banana", "Mango"]
+stringList2=[i[0] for i in stringList]
+print(stringList2)
+#p13
+'''Given a list of words, create a new list containing only those words that are
+shorter than 4 characters. For example, if the list is [“Ant”, “Dog”, “Lion”, ”Fish”]), the
+new list should be [“Ant”, “Dog”]'''
+WordList= ["Ant","Lion","Dog","Fish"]
+WordList2=[i for i in WordList if len(i)<4]
+print(WordList2)
